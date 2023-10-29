63eb9dd2cf92fb8400be853d543a6a0c8bee562c
Satish <satishgowda28@gmail.com>
2023-10-29 17:43:49 +0530

    book management complete

--- go_book-management_db/cmd/main/main.go
+++ go_book-management_db/cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/satishgowda28/go_projects/go_book-management_db/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterRoutes(r)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
